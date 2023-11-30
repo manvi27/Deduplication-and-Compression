e06b6c4e942bb89c9bbf257b3990453b5e26963e
manvi27 <manviaga27@gmail.com>
2023-11-29 23:52:47 -0500

    Added pin to cpu related func

--- App/Utilities.cpp
+++ App/Utilities.cpp
@@ -104,3 +104,40 @@ void set_callback(cl::Event event, const char *queue_name) {
   OCL_CHECK(err,
             err = event.setCallback(CL_COMPLETE, event_cb, (void *)queue_name));
 }
+
+// from https://eli.thegreenplace.net/2016/c11-threads-affinity-and-hyperthreading/
+void pin_thread_to_cpu(std::thread &t, int cpu_num)
+{
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__) || defined(__APPLE__)
+    return;
+#else
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(cpu_num, &cpuset);
+    int rc =
+        pthread_setaffinity_np(t.native_handle(), sizeof(cpu_set_t), &cpuset);
+    if (rc != 0)
+    {
+        std::cerr << "Error calling pthread_setaffinity_np: " << rc << "\n";
+    }
+#endif
+}
+
+void pin_main_thread_to_cpu0()
+{
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__) || defined(__APPLE__)
+    return;
+#else
+    pthread_t thread;
+    thread = pthread_self();
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(0, &cpuset);
+    int rc =
+        pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+    if (rc != 0)
+    {
+        std::cerr << "Error calling pthread_setaffinity_np: " << rc << "\n";
+    }
+#endif
+}
